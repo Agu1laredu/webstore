a32c67c0d8d8d0166549961e8413bb04c04c5cc7
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-05-08 15:54:49 -0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,33 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppService } from './app.service';
+import { Product } from './products/entities/product.entity';
 import { ProductsModule } from './products/products.module';
 
+require('detenv').config();
+
+const db_options = {
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+};
+
+console.log(db_options);
+
 @Module({
-  imports: [ProductsModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      ...db_options,
+      entities: [Product],
+      synchronize: true,
+    }),
+    ProductsModule,
+  ],
+
   controllers: [AppController],
   providers: [AppService],
 })
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: ['http://localhost:3000'],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   });
   app.setGlobalPrefix('api');
-  await app.listen(3001);
+  await app.listen(port);
 }
 bootstrap();
