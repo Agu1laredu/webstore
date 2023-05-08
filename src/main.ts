7726ac3423bba7b2763ee4ac0b2aa5e13f948f39
Eduardo Aguilar <Agu1laredu@hotmail.com>
2023-05-08 17:37:50 -0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -2,13 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const port = process.env.PORT || 3000;
   const app = await NestFactory.create(AppModule);
-  app.enableCors({
-    origin: ['http://localhost:3000'],
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  });
-  app.setGlobalPrefix('api');
-  await app.listen(port);
+  await app.listen(3001);
 }
 bootstrap();
